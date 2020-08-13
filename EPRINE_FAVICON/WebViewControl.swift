707aa0717ed1ea676f93088d73cc0ea420047aaa
ashishlaw <ashishl@adaptivetelehealth.com>
2020-08-13 21:39:29 +0530

    Zoom redirect

--- EPRINE_FAVICON/WebViewControl.swift
+++ EPRINE_FAVICON/WebViewControl.swift
@@ -11,7 +11,7 @@ import WebKit
 import PKHUD
 import OneSignal
 
-class WebViewControl: UIViewController, UIWebViewDelegate, WKNavigationDelegate {
+class WebViewControl: UIViewController, UIWebViewDelegate, WKNavigationDelegate , WKScriptMessageHandler {
     
     @IBOutlet weak var webView: WKWebView!
     
@@ -41,9 +41,16 @@ class WebViewControl: UIViewController, UIWebViewDelegate, WKNavigationDelegate 
             }
         })
         
+        
+        let theConfiguration = WKWebViewConfiguration()
+        theConfiguration.userContentController.add(
+            self,
+            name: "myApp")
+
+        
         url = "https://eprine.test.adaptivetelehealth.com/index.php/login/mobile_login/\(UserStore.shared.token)/\(UserStore.shared.deviceToken)"
         print("url", url)
-        let webKit = WKWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        let webKit = WKWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height) ,configuration: theConfiguration)
         webKit.load(URLRequest(url: URL(string: url)!))
         webKit.navigationDelegate = self
         webKit.tag = 1111
@@ -59,9 +66,9 @@ class WebViewControl: UIViewController, UIWebViewDelegate, WKNavigationDelegate 
             }
            // webView.stringByEvaluatingJavaScript(from: "document.getElementById('idLoginButton').click()")
             webView.evaluateJavaScript("document.getElementById('idLoginButton').click()", completionHandler: nil)
-            if webView.url?.relativeString == "https://zvideo.adaptivetelehealth.com/?API_KEY=" {
-                print("url matched")
-            }
+        //    if webView.url?.relativeString == "https://zvideo.adaptivetelehealth.com/?API_KEY=" {
+          //      print("url matched")
+            //}
             
             let hitURL = "https://eprine.test.adaptivetelehealth.com/index.php/login"
            // let meetingURL = "https://zvideo.adaptivetelehealth.com/?API_KEY=4q6_BakLTE-IPKB3nhSG_A&signature=NHE2X0Jha0xURS1JUEtCM25oU0dfQS4xMTMxMTA1ODkuMTU4NTI5Mzg2OTAwMC4xLkJiZTkxaXZPUkF1RlpHUUxGeUpFY254c2dHdFI0Z1RVWGZDNlIxK0RKdEU9&name=Anne++Smith&meeting_number=113110589&company_name=&redirect_url=https%3A%2F%2Fabundant.adaptivetelehealth.com%2F"
@@ -99,4 +106,87 @@ class WebViewControl: UIViewController, UIWebViewDelegate, WKNavigationDelegate 
                 }
             }
         }
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+       decisionHandler(.allow)
+       guard let urlAsString = navigationAction.request.url?.absoluteString.lowercased() else {
+           return
+       }
+
+      print("navigationAction ",urlAsString)
+        
+        let zoomBaseURl = "zoomus://zoom.us/start?confno="
+        let token = "?token="
+        let url = URL.init(string: urlAsString)
+        let paramters = url?.queryParameters
+        
+        
+        if urlAsString.contains("zoom.us/s"){
+            print("Is zoom")
+           // guard let zoomId = urlAsString.slice(from: "adaptivetelehealth.zoom.us/s/", to: "?zak=")else {return}
+            guard let zoomId = paramters?["confno"] , let zak = paramters?["zak"] else {return}
+            print("zoomId ",zoomId)
+            let mainUrl = "\(zoomBaseURl)\(zoomId)&token=\(zak)"
+            print("Main url ",mainUrl)
+            if UIApplication.shared.canOpenURL(URL.init(string: mainUrl)!) {
+                UIApplication.shared.open(URL.init(string: mainUrl)!, options: [:], completionHandler: nil)
+            }
+        }
+//        else if urlAsString.contains("zoom.us/j"){
+//                   print("Is zoom")
+//                   guard let zoomId = urlAsString.slice(from: "adaptivetelehealth.zoom.us/j/", to: "?zak=")else {return}
+//                   print("zoomId ",zoomId)
+//            let tokenURL = token + ""
+//                  let mainUrl = "\(zoomBaseURl)\(zoomId)"
+//
+//                   print("Main url ",mainUrl)
+//                    if UIApplication.shared.canOpenURL(URL.init(string: mainUrl)!) {
+//                                  UIApplication.shared.open(URL.init(string: mainUrl)!, options: [:], completionHandler: nil)
+//                              }
+//               }
+        
+    }
+    
+//zoomus://zoom.us/join?confno=94468084778&token=eyj6bv9za20ioij6bv9vmm0ilcjhbgcioijiuzi1niisinr5cci6ikpxvcj9.eyjhdwqioijjbgllbnqilcj1awqioijgejdpmnzud1jjt1rurw5zbgh3dgjniiwiaxnzijoid2viiiwic3r5ijo5oswid2nkijoiyxcxiiwiy2x0ijowlcjzdgsioijiodvuuzvfctawmlr6uuctx2pwng1jvfewr0nwsnm1zxjndeh2dmtgefzzlkjnvtrrm2n6uvvwu1yyefhwmnbmtvhssmffzdzlsg95wtnssgryqkznwef2u1u5b1riqxlowfjruvrsnfrtefvzblzazda4ek5ddg5qvdbbquf3elewskjkvzlwv1znemn6mefbmkyztveilcjlehaioje2mduwotuxndesimlhdci6mtu5nzmxote0mswiywlkijoirtfctjd5tvfteg1iu3vqum9nelhsusisimnpzci6iij9.mlv5gdgbu5n-4ckdpxiiesov6kz0u8k3oxhhrxpgejw
+    
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage){
+        
+        print("Script message ",message.body)
+        
+        
+        
+        
+    }
+
+    
+    
+    
+    
 }
+
+extension String {
+
+    func slice(from: String, to: String) -> String? {
+
+        return (range(of: from)?.upperBound).flatMap { substringFrom in
+            (range(of: to, range: substringFrom..<endIndex)?.lowerBound).map { substringTo in
+                substring(with: substringFrom..<substringTo)
+            }
+        }
+    }
+}
+
+
+extension URL {
+    public var queryParameters: [String: String]? {
+        guard
+            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems else { return nil }
+        return queryItems.reduce(into: [String: String]()) { (result, item) in
+            result[item.name] = item.value
+        }
+    }
+}
+
